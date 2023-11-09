cfdf6645789f4c7aa32db668135078877d60f44e
Jules <dali1999@naver.com>
2023-11-10 03:09:24 +0900

    feat(printBenefitOpening):이벤트 소개 멘트 출력 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -9,6 +9,13 @@ const OutputView = {
   printOpening() {
     Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
+
+  printBenefitOpening() {
+    const date = InputView.visitDate;
+    Console.print(
+      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+    );
+  },
 };
 
 // const m = new Menu();
