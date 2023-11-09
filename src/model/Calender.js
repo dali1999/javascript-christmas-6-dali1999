cd53d2f7ed1f5edada2decdcb1bb7c9b6dca1c5c
Jules <dali1999@naver.com>
2023-11-10 06:04:50 +0900

    feat(storeSpecialDate): 'star'이 포함된 날짜들을 배열에 저장하는 기능 추가

--- src/model/Calender.js
+++ src/model/Calender.js
@@ -1,40 +1,56 @@
 import { Console } from '@woowacourse/mission-utils';
+import OutputView from '../view/OutputView.js';
+import Menu from './Menu.js';
 
 class Calender {
-  generateDecemberCalendar(year) {
+  constructor() {
+    this.menu = new Menu();
+    this.specialDate = [];
+    this.weekDay = [];
+    this.weekendDay = [];
+  }
+
+  static generateDecemberCalendar(year) {
     const calendar = [];
 
     // 12월의 첫 날부터 마지막 날까지 반복
-    for (let day = 1; day <= 31; day++) {
+    for (let day = 1; day <= 31; day += 1) {
       const currentDate = new Date(year, 11, day);
-
       // 현재 날짜가 12월이 아니라면 반복 중지
       if (currentDate.getMonth() !== 11) {
         break;
       }
-
       // 날짜와 요일 정보를 포함하는 객체 생성
       const dayInfo = {
         day,
         dayOfWeek: currentDate.getDay(), // 0은 일요일, 1은 월요일, ...,5은 금요일, 6은 토요일
       };
-
       // 25일과 일요일에 'star' 추가
       if (day === 25 || dayInfo.dayOfWeek === 0) {
         dayInfo.specialEvent = 'star';
       }
-
       // 달력에 날짜 정보 추가
       calendar.push(dayInfo);
     }
-
     return calendar;
   }
+
+  // 별 있는 날짜 저장하는 배열 [ 3, 10, 17, 24, 25, 31 ]
+  storeSpecialDate() {
+    const december = Calender.generateDecemberCalendar(2023);
+    december.forEach((dayInfo) => {
+      if (dayInfo.specialEvent) this.specialDate.push(dayInfo.day);
+    });
+    return this.specialDate;
+  }
+
+
 }
-// const calender = new Calender();
-// const december = calender.generateDecemberCalendar(2023);
-// Console.print(december);
-// december.forEach((dayInfo) => {
-//   if (dayInfo.specialEvent) Console.print(dayInfo.day);
-// });
+const calender = new Calender();
+
+// const december = Calender.generateDecemberCalendar(2023);
+// Console.print(calender.storeSpecialDate());
+// Console.print(calender.storeWeekday());
+// Console.print(calender.storeWeekendday());
+
 export default Calender;
