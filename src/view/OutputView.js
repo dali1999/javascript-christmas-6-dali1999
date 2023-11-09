4060769f8e67f2bb4e34395e77ba18f350321385
Jules <dali1999@naver.com>
2023-11-10 03:10:35 +0900

    feat(printOrderMenu): 주문 메뉴 출력하는 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -16,6 +16,23 @@ const OutputView = {
       `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
     );
   },
+
+  printOrderMenu() {
+    Console.print('<주문 메뉴>');
+    const orderMenuArr = InputView.orderMenu.split(',');
+    this.formattedMenuArr = orderMenuArr.map((item) => {
+      const [menu, num] = item.split('-');
+
+      if (!this.newMenu.findMenuByName(menu.trim())) {
+        throw new Error('[ERROR] 메뉴에 없습니다');
+      }
+
+      Console.print(`${menu.trim()} ${num.trim()}개`);
+      return [menu.trim(), parseInt(num.trim(), 10)];
+    });
+    // Console.print(orderMenuArr); // ['a-2', 'b-3']
+    // Console.print(this.formattedMenuArr); // [ ['a', 2],['b', 3] ]
+  },
 };
 
 // const m = new Menu();
