68392b9a88f8ee4f61aa46168fb940b2c5a726af
Jules <dali1999@naver.com>
2023-11-10 06:33:20 +0900

    fix(visitDate): '방문 날짜' 입력 타입 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,21 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
-  visitDate: null,
-  orderMenu: null,
+  visitDate: 0,
+  orderMenu: [],
 
   async readVisitDate() {
     this.visitDate = await Console.readLineAsync(
       '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
     );
-    return this.visitDate;
+    this.visitDate = parseInt(this.visitDate, 10);
   },
 
   async readOrderMenu() {
     this.orderMenu = await Console.readLineAsync(
       '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
     );
-    return this.orderMenu;
   },
 };
 
