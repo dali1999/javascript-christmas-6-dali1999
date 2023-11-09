6492c16992451f00849295b36ddfa94ad5108144
Jules <dali1999@naver.com>
2023-11-10 02:21:42 +0900

    feat(readOrderMenu): '주문 메뉴'입력 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,42 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import InputView from './InputView.js';
+import Menu from '../model/Menu.js';
+
+const OutputView = {
+  formattedMenuArr: [],
+  newMenu: new Menu(),
+  printOpening() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+  },
+
+  printBenefitOpening() {
+    const date = InputView.visitDate;
+    Console.print(
+      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
+    );
+  },
+
+  printOrderMenu() {
+    Console.print('<주문 메뉴>');
+    // const newMenu = new Menu();
+    const orderMenuArr = InputView.orderMenu.split(',');
+    this.formattedMenuArr = orderMenuArr.map((item) => {
+      const [menu, num] = item.split('-');
+      if (!this.newMenu.findMenuByName(menu.trim())) {
+        throw new Error('없어!');
+      }
+      Console.print(`${menu.trim()} ${num.trim()}개`);
+      return [menu, parseInt(num, 10)];
+    });
+    Console.print(this.formattedMenuArr);
+  },
+
+  printTotalPriceBeforeDiscount() {
+    Console.print(this.formattedMenuArr);
+    Console.print(this.newMenu.getPriceForMenu());
+  },
+};
+
+// const m = new Menu();
+// Console.print(m.);
+export default OutputView;
